e8d9dc5f8fd666f96f594009b444402ed4868fea
Friedrich von Never <friedrich@fornever.me>
2021-06-05 20:41:20 +0700

    ShapeFile: initial shape file structure info (#13)

---
+++ Overtone.Extractor/Program.fs
@@ -0,0 +1,20 @@
+﻿open System.IO
+
+open Overtone.Resources.Shape
+
+[<EntryPoint>]
+let main: string[] -> int = function
+| [|"info"; inputFilePath|] ->
+    use stream = new FileStream(inputFilePath, FileMode.Open)
+    let file = ShapeFile stream
+    printfn $"Shape file %s{Path.GetFileName inputFilePath}"
+    let sprites = file.ReadSprites() |> Seq.toArray
+    printfn $"Sprite count: %d{sprites.Length}"
+    sprites |> Array.iteri (fun i sprite ->
+        let palette = if Option.isSome sprite.PaletteOffset then "present" else "absent"
+        printfn $"Sprite %d{i}. Offset %d{sprite.SpriteOffset}, custom palette: %s{palette}"
+    )
+    0
+| _ ->
+    printfn "Usage:\ninfo <path-to-shp-file>: print shp file info"
+    1
---
+++ Overtone.Resources/Shape.fs
@@ -0,0 +1,28 @@
+﻿namespace Overtone.Resources.Shape
+
+open System
+open System.IO
+open System.Text
+
+type SpriteData = {
+    SpriteOffset: int
+    PaletteOffset: int option
+}
+
+type ShapeFile(input: Stream) =
+    member _.ReadSprites(): SpriteData seq =
+        input.Seek(0L, SeekOrigin.Begin) |> ignore
+        use reader = new BinaryReader(input, Encoding.UTF8, leaveOpen = true)
+
+        let headerBytes = reader.ReadBytes 4
+        let header = Encoding.UTF8.GetString headerBytes
+        if header <> "1.10" then failwithf $"Invalid shape file header: %s{BitConverter.ToString headerBytes}"
+
+        let spriteCount = reader.ReadInt32()
+        upcast Array.init spriteCount (fun _ -> {
+            SpriteOffset = reader.ReadInt32()
+            PaletteOffset =
+                match reader.ReadInt32() with
+                    | 0 -> None
+                    | offset -> Some offset
+        })

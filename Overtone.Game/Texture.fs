40b42cf2aab0c4884897296ab8ee2e90e8dac7c9
Friedrich von Never <friedrich@fornever.me>
2022-02-27 21:22:00 +0700

    (#21) [UNFINISHED] Resources: initial commit for game disc

---
+++ Overtone.Game/Texture.fs
@@ -0,0 +1,26 @@
+﻿module Overtone.Game.Texture
+
+open System
+open System.IO
+open Microsoft.Xna.Framework.Graphics
+
+open Overtone.Resources
+open Overtone.Resources.Cob
+open Overtone.Resources.Shape
+
+let loadShape (disc: GameDisc) (name: string): Texture2D =
+    use stream = new FileStream(disc.DataArchivePath)
+    use cob = new CobFile(stream)
+
+    let readFile dataName =
+        cob.ReadEntries()
+        |> Seq.filter(fun x -> x.Path = dataName)
+        |> Seq.exactlyOne
+        |> cob.ReadEntry
+
+    let shapeData = readFile name
+    let paletteName = ShapePalette.get()
+
+    use stream = new MemoryStream(data)
+    let shape = ShapeFile(stream)
+    shape.Render
---
+++ Overtone.Resources/GameDisc.fs
@@ -0,0 +1,6 @@
+﻿namespace Overtone.Resources
+
+open System.IO
+
+type GameDisc(rootPath: string) =
+    member val DataArchivePath: string = Path.Combine(rootPath, "THING1", "TONE01.COB")

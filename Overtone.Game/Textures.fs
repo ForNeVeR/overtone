b11301ffd5b1216bbd42e7b34fae72d839e72bee
Fadoli <f13@laposte.net>
2023-09-18 08:41:26 +0200

    WIP: don't alloc the whole surface, offset texture

--- Overtone.Game/Textures.fs
+++ Overtone.Game/Textures.fs
@@ -11,52 +11,63 @@ open Overtone.Game.Config
 open Overtone.Resources
 open Overtone.Utils
 
-let toTexture (bitmap: SKBitmap, lifetime: Lifetime, device: GraphicsDevice): Texture2D =
+let toTexture (bitmap: SKBitmap, lifetime: Lifetime, device: GraphicsDevice) : Texture2D =
     let width = bitmap.Width
     let height = bitmap.Height
     let texture = new Texture2D(device, width, height) |> Lifetimes.attach lifetime
-    let colors = Array.init (width * height) (fun i ->
-        let x = i % width
-        let y = i / width
-        let drawingColor = bitmap.GetPixel(x, y)
-        Color(
-            r = int drawingColor.Red,
-            g = int drawingColor.Green,
-            b = int drawingColor.Blue,
-            alpha = int drawingColor.Alpha
-        )
-    )
+
+    let colors =
+        Array.init (width * height) (fun i ->
+            let x = i % width
+            let y = i / width
+            let drawingColor = bitmap.GetPixel(x, y)
+
+            Color(
+                r = int drawingColor.Red,
+                g = int drawingColor.Green,
+                b = int drawingColor.Blue,
+                alpha = int drawingColor.Alpha
+            ))
+
     texture.SetData colors
     texture
 
+type Texture2DWithOffset (texture:Texture2D, offsetX:int, offsetY:int)=
+    member _.texture=texture
+    member _.offsetX=offsetX
+    member _.offsetY=offsetY
+    member _.offset:Vector2 = Vector2(float32(offsetX),float32(offsetY))
+
 type Manager(disc: GameDisc, device: GraphicsDevice, config: ShapesConfiguration) =
 
-    let mutable InternalCache: Map<string, Texture2D[]> = Map.empty;
+    let mutable InternalCache: Map<string, Texture2DWithOffset[]> = Map.empty
 
-    member _.LoadWholeShape(lifetime: Lifetime, shapeId: string): Texture2D[] =
-        let key = shapeId;
-        printfn "Try loading : %s" shapeId
+    member _.LoadWholeShape(lifetime: Lifetime, shapeId: string) : Texture2DWithOffset[] =
+        let key = shapeId
+
         if not (InternalCache.ContainsKey key) then
             let shapeName = config.GetShapeName shapeId
             use shapeStream = new MemoryStream(disc.GetData shapeName)
             let shape = Shape.ShapeFile shapeStream
 
-            let paletteName = ShapePalette.getWithDisk(shapeName,disc)
+            let paletteName = ShapePalette.getWithDisk (shapeName, disc)
+
             let palette =
                 use paletteStream = new MemoryStream(disc.GetData paletteName)
                 Palette.Read paletteStream
 
             // TODO[#30]: Better resource management, do not read the whole shape again for every new sprite
-            let array = 
+            let array =
                 shape.ReadSpriteHeaders()
                 |> Seq.map (fun header -> shape.ReadSprite header)
-                |> Seq.map (fun sprite -> shape.Render palette sprite)
-                |> Seq.map (fun bitmap -> toTexture(bitmap, lifetime, device))
+                // |> Seq.map (fun sprite -> shape.Render palette sprite)
+                |> Seq.map (fun sprite -> shape.SmallRender palette sprite)
+                |> Seq.map (fun bitmap -> Texture2DWithOffset(toTexture (bitmap.bitmap, lifetime, device), bitmap.offsetX, bitmap.offsetY))
                 |> Seq.toArray
-            InternalCache <- InternalCache.Add(key,array)
-        else
-            printfn "Entry was in cache !"
+
+            InternalCache <- InternalCache.Add(key, array)
+
         InternalCache[key]
 
-    member this.LoadTexture(lifetime: Lifetime, shapeId: string, spriteIndex: int): Texture2D =
-        this.LoadWholeShape(lifetime,shapeId)[spriteIndex]
+    member this.LoadTexture(lifetime: Lifetime, shapeId: string, spriteIndex: int) : Texture2DWithOffset =
+        this.LoadWholeShape(lifetime, shapeId)[spriteIndex]

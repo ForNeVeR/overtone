336693d540f08e7ba7174acd95c5ce6ba396a017
Fadoli <f13@laposte.net>
2023-09-20 00:24:32 +0200

    [PERFORMANCE/READABILITY] clean up line parsing

--- Overtone.Resources/TextConfiguration.fs
+++ Overtone.Resources/TextConfiguration.fs
@@ -4,18 +4,14 @@ open System
 open System.IO
 open System.Text
 
+let commentsChar= "/;".ToCharArray()
+
 let private clearLine(line: string) =
-    let toOption int = if int < 0 then ValueNone else ValueSome int
-    let ``;`` = line.IndexOf ';' |> toOption
-    let ``//`` = line.IndexOf "//" |> toOption
-    let commentPos =
-        match ``;``, ``//`` with
-        | ValueNone, x -> x
-        | x, ValueNone -> x
-        | ValueSome x, ValueSome y -> ValueSome <| min x y
-    match commentPos with
-    | ValueNone -> line
-    | ValueSome pos -> line.Substring(0, pos)
+    let commentStart = line.IndexOfAny(commentsChar)
+    if commentStart <> -1 then
+        line.Substring(0, commentStart)
+    else
+        line
 
 let readKeyValueEntry(line: string): string*string =
     let components = line.Split([| ' '; '\t' |], 2, StringSplitOptions.RemoveEmptyEntries)
@@ -29,7 +25,7 @@ let extractLines(data: byte[]): string seq = seq {
     use reader = new StreamReader(stream, Encoding.UTF8)
 
     let mutable line = reader.ReadLine()
-    while not <| isNull line do
+    while not (isNull line) do
         let withoutComments = (clearLine line).TrimEnd()
         if withoutComments <> "" then
             yield withoutComments

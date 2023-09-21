fdebd805318e2cd3301276660227f80b72f5c2f7
Fadoli <f13@laposte.net>
2023-09-21 13:32:31 +0200

    Documentation : Island formats
    add indication

--- Overtone.Game/Config/IslandsConfiguration.fs
+++ Overtone.Game/Config/IslandsConfiguration.fs
@@ -2,6 +2,22 @@ namespace Overtone.Game.Config
 
 open Overtone.Resources
 
+//
+// HIGH LEVEL FILE FORMAT
+//
+// SIZE_COUNT
+//
+// FOREACH ENTRY: (SIZE_COUNT)
+//    ISLAND_COUNT
+//    FOREACH ISLAND: (ISLAND_COUNT)
+//       ANGLE RANGE 0 SHAPEID (SERVES AS MAPID !)
+//    END
+//    UNTIL -1: (BRIDGE DEFINITIONS)
+//       ISLAND_FROM ISLAND_TO
+//    END
+// END
+//
+
 type IslandsConfiguration(names: Map<string, string>) =
 
     static member Read(shapesTxt: byte[]): IslandsConfiguration =

2bb22f57bac0a235def05b0d57f02e3face1edd0
Fadoli <f13@laposte.net>
2023-09-25 00:46:15 +0200

    Sound : hardcode button sound into the game

--- Overtone.Game/Program.fs
+++ Overtone.Game/Program.fs
@@ -7,16 +7,17 @@ open Overtone.Resources
 let main(args: string[]): int =
     let discRoot = args[0]
 
-    use disc = new GameDisc(discRoot)
+    GameState.discRoot <- discRoot
+    GameState.init(discRoot)
     let islands = new IslandsConfiguration()
-    islands.Read <| disc.GetData "data\\worldpos.txt"
+    islands.Read <| GameState.getDisc().GetData "data\\worldpos.txt"
 
-    let soundsConfig = SoundsConfiguration.Read <| disc.GetConfig "sound.txt"
-    let shapesConfig = ShapesConfiguration.Read <| disc.GetConfig "shapes.txt"
-    let windowsConfig = WindowsConfiguration.Read <| disc.GetConfig "windows.txt"
+    let soundsConfig = SoundsConfiguration.Read <| GameState.getDisc().GetConfig "sound.txt"
+    let shapesConfig = ShapesConfiguration.Read <| GameState.getDisc().GetConfig "shapes.txt"
+    let windowsConfig = WindowsConfiguration.Read <| GameState.getDisc().GetConfig "windows.txt"
 
-    use game = new OvertoneGame(disc, shapesConfig, windowsConfig)
-    let mainTheme = soundsConfig.GetSoundPerName("START.WAV",disc)
+    use game = new OvertoneGame(GameState.getDisc(), shapesConfig, windowsConfig)
+    let mainTheme = soundsConfig.GetSoundPerName("START.WAV",GameState.getDisc())
     mainTheme.Play() |> ignore
     game.Run()
     0

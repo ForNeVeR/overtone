00c03dd4b27346709e2096f928f92459c6351909
Friedrich von Never <friedrich@fornever.me>
2024-09-28 13:36:56 +0200

    (#6) Video: basic investigation using Accord.NET

---
+++ Overtone.Video/Program.fs
@@ -0,0 +1,17 @@
+﻿open Accord.Video.VFW
+
+let filePath = @"D:\Games\TONE\INTRO.AVI"
+
+[<EntryPoint>]
+let main (_: string[]): int =
+    use aviFile = new AVIReader()
+    aviFile.Open filePath
+    printfn $"%s{aviFile.Codec}"
+    let f = aviFile.GetNextFrame()
+    let f = aviFile.GetNextFrame()
+    let f = aviFile.GetNextFrame()
+    let f = aviFile.GetNextFrame()
+    let f = aviFile.GetNextFrame()
+    let f = aviFile.GetNextFrame()
+    f.Save @"T:\Temp\frame01.bmp"
+    0

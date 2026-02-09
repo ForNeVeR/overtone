a4084711e4a87e4f417b56d61a0fc6114b6631ff
Friedrich von Never <friedrich@fornever.me>
2026-02-09 23:30:36 +0100

    Sound: push loading up to Controls.Load

--- Overtone.Game/UI/Controls.fs
+++ Overtone.Game/UI/Controls.fs
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2023-2025 Overtone contributors <https://github.com/ForNeVeR/overtone>
+// SPDX-FileCopyrightText: 2023-2026 Overtone contributors <https://github.com/ForNeVeR/overtone>
 //
 // SPDX-License-Identifier: MIT
 
@@ -6,10 +6,10 @@ namespace Overtone.Game.UI
 
 open JetBrains.Lifetimes
 
+open Overtone.Game
 open Overtone.Game.Config
 open Overtone.Utils.Constants
 open Overtone.Game.Textures
-
 
 module Controls =
     let Load (lifetime: Lifetime, textureManager: Manager, entry: WindowEntry): IDrawableUI =
@@ -19,7 +19,12 @@ module Controls =
 
         if entry.MouseFocus && entry.WindowType = WindowTypes.Button then
             let hoverTexture = textureManager.LoadTexture(lifetime, entry.ShapeId, entry.ShapeFrame + 1)
-            Button(normalTexture, hoverTexture, entry)
+            Button(
+                normalTexture,
+                hoverTexture,
+                entry,
+                GameState.soundsConfig.GetSoundPerName("BUTTON", GameState.getDisc()) // TODO: Migrate to the SoundManager, only load once
+            )
         else if entry.MouseFocus && entry.WindowType = WindowTypes.DropDownMenu then
             DropDownMenuButton(normalTexture, entry)
         else
